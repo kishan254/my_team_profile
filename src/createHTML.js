f0b856ee74f739e130c31e9312ac35ae0fd779e9
Kishan Gosrani <kishan.gosrani22@gmail.com>
2022-04-13 15:55:36 +0800

    creatHTML file update

--- src/createHTML.js
+++ src/createHTML.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 // Initial create HTML function, pluging in the generated cards with the template lteral.
 
 function createHTML(data){
-    retun `
-    <!DOCTYPE html>
+    return `
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,103 +13,43 @@ function createHTML(data){
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
     <title>My Team Generator</title>
 </head>
-<body>
-    <style>
-        body {
-            box-sizing: border-box;
-        }
-        .container {
-            display:flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-        }
+    <body>
+        <style>
+            body {
+                box-sizing: border-box;
+            }
+            .container {
+                display:flex;
+                flex-wrap: wrap;
+                justify-content: space-between;
+            }
 
-        #project {
-            background: cornsilk;
-            color: palevioletred;
-        }
+            #project {
+                background: cornsilk;
+                color: palevioletred;
+            }
 
-        #person {
-            background: cornsilk;
-            color: darkorange;
-        }
+            #person {
+                background: cornsilk;
+                color: darkorange;
+            }
 
-        .subject {
-            font-size: larger;
-            font-weight: bolder;
-        }
-    </style>
+            .subject {
+                font-size: larger;
+                font-weight: bolder;
+            }
+        </style>
 
-    <div class = "jumbortron" id="project">
-        <h1 class = "text-center">Our Development Team</h1>
-    </div>
-
-    <div class = "container">
-
-        <!--Card for Manager-->
-        <div class = "card cardbody">
-            <div class = "card-header" id = "person">
-                <p class = "subject">Kishan</p>
-                <div>Manager</div>
-            </div>
-            <div class="card-body">
-                <form role="form">
-                    <div class="form-group">
-                        <label>ID: 20</label>
-                    </div>
-                    <div class="form-group">
-                        <label>Email: <a href="mailto:kishan.gosrani@hotmail.com">kishan.gosrani@hotmail.com</a></label>
-                    </div>
-                    <div class="form-group">
-                        <label>Office Line Extention: 200 </label>
-                    </div>
-                </form>
-            </div>
+        <div class = "jumbortron" id="project">
+            <h1 class = "text-center">Our Development Team</h1>
         </div>
-        <!--Card for Intern-->
-        <div class = "card cardbody">
-            <div class = "card-header" id = "person">
-                <p class = "subject">Marco</p>
-                <div>Intern</div>
-            </div>
-            <div class="card-body">
-                <form role="form">
-                    <div class="form-group">
-                        <label>ID: 21</label>
-                    </div>
-                    <div class="form-group">
-                        <label>Email: <a href="mailto:marco@marco.com">marco@marco.com</a></label>
-                    </div>
-                    <div class="form-group">
-                        <label>School: UWA </label>
-                    </div>
-                </form>
-            </div>
+
+        <div class = "container">
+            ${allCards(data)}
         </div>
-        <!--Card for Engineer-->
-        <div class = "card cardbody">
-            <div class = "card-header" id = "person">
-                <p class = "subject">Luca</p>
-                <div>Engineer</div>
-            </div>
-            <div class="card-body">
-                <form role="form">
-                    <div class="form-group">
-                        <label>ID: 22</label>
-                    </div>
-                    <div class="form-group">
-                        <label>Email: <a href="mailto:luca@luca.com">luca@luca.com</a></label>
-                    </div>
-                    <div class="form-group">
-                        <label>Github: <a href="https://github.com/kishan254">Github</a></label>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
+    </body>
 </html>
-    `
+`
 }
 
 //Function to determine role being added, then calling the correct function
@@ -136,19 +76,19 @@ function createManager(data) {
     return `
         <div class = "card cardbody">
             <div class = "card-header" id = "person">
-                <p class = "subject">Kishan</p>
-                <div>Manager</div>
+                <p class = "subject">${data.name}</p>
+                <div>${data.getRole()}</div>
             </div>
             <div class="card-body">
                 <form role="form">
                     <div class="form-group">
-                        <label>ID: 20</label>
+                        <label>ID: ${data.id}</label>
                     </div>
                     <div class="form-group">
-                        <label>Email: <a href="mailto:kishan.gosrani@hotmail.com">kishan.gosrani@hotmail.com</a></label>
+                        <label>Email: <a href="mailto:${data.email}">${data.email}</a></label>
                     </div>
                     <div class="form-group">
-                        <label>Office Line Extention: 200 </label>
+                        <label>Office Line Extention: ${data.officeNumber} </label>
                     </div>
                 </form>
             </div>
@@ -161,19 +101,19 @@ function createIntern(data);{
     return `
     <div class = "card cardbody">
             <div class = "card-header" id = "person">
-                <p class = "subject">Marco</p>
-                <div>Intern</div>
+                <p class = "subject">${data.name}</p>
+                <div>${data.getRole()}</div>
             </div>
             <div class="card-body">
                 <form role="form">
                     <div class="form-group">
-                        <label>ID: 21</label>
+                        <label>ID: ${data.id}</label>
                     </div>
                     <div class="form-group">
-                        <label>Email: <a href="mailto:marco@marco.com">marco@marco.com</a></label>
+                        <label>Email: <a href="mailto:${data.email}">${data.email}</a></label>
                     </div>
                     <div class="form-group">
-                        <label>School: UWA </label>
+                        <label>School: ${data.school} </label>
                     </div>
                 </form>
             </div>
@@ -186,19 +126,19 @@ function createEngineer(data) {
     return `
     <div class = "card cardbody">
             <div class = "card-header" id = "person">
-                <p class = "subject">Luca</p>
-                <div>Engineer</div>
+                <p class = "subject">${data.name}</p>
+                <div>${data.getRole()}</div>
             </div>
             <div class="card-body">
                 <form role="form">
                     <div class="form-group">
-                        <label>ID: 22</label>
+                        <label>ID: ${data.id}</label>
                     </div>
                     <div class="form-group">
-                        <label>Email: <a href="mailto:luca@luca.com">luca@luca.com</a></label>
+                        <label>Email: <a href="mailto:${data.email}">${data.email}</a></label>
                     </div>
                     <div class="form-group">
-                        <label>Github: <a href="https://github.com/kishan254">Github</a></label>
+                        <label>Github: <a href="https://github.com/${data.github}">Github</a></label>
                     </div>
                 </form>
             </div>
